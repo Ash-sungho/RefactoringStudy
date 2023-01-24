5d3b9f78774b9ac6909ce57ea8da0a10aede0853
SUNGHO-DESKTOP\andyk <andykr1234@gmail.com>
2023-01-25 00:42:02 +0900

    함수 추출하기

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,31 +1,40 @@
 export function printOwing(invoice) {
-  let outstanding = 0;
-
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
+  printBanner();
 
   // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
+  let outstanding = calculateOutstanding(invoice);
 
   // record due date
+  recordDueDate(invoice);
+
+  //print details
+  printDetails(invoice, outstanding);
+}
+
+const printBanner = () => {
+  console.log("***********************");
+  console.log("**** Customer Owes ****");
+  console.log("***********************");
+};
+const calculateOutstanding = (invoice) => {
+  return invoice.orders.reduce((sum, orders) => (sum += orders.amount), 0);
+};
+const recordDueDate = (invoice) => {
   const today = new Date();
   invoice.dueDate = new Date(
     today.getFullYear(),
     today.getMonth(),
     today.getDate() + 30
   );
-
-  //print details
+};
+const printDetails = (invoice, outstanding) => {
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
-}
+};
 
 const invoice = {
   orders: [{ amount: 2 }, { amount: 5 }],
-  customer: '엘리',
+  customer: "엘리",
 };
 printOwing(invoice);
